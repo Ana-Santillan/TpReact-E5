ee9dee0d852039ea10f74924f54c50b00e9b135e
Ana Santillan <anisantillan10@gmail.com>
2023-06-01 19:46:43 -0300

    se agrego logica para almacenar las tareas en el local storage

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,31 +1,36 @@
 import { Button, Form } from "react-bootstrap";
 import Lista from "./Lista";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Formulario = () => {
+    const tareasLS = JSON.parse(localStorage.getItem('listaTareas')) || [];
     const [tarea, setTarea] = useState("");
-    const [tareas, setTareas] = useState([]);
+    const [tareas, setTareas] = useState(tareasLS);
 
-    const handleSubmit = (e)=>{
+    useEffect(() => {
+        localStorage.setItem('listaTareas', JSON.stringify(tareas));
+    }, [tareas])
+
+    const handleSubmit = (e) => {
         e.preventDefault();
         setTareas([...tareas, tarea])
         setTarea("")
     }
 
-    const borrarTarea = (nombreTarea)=>{
-        let copiaTareas = tareas.filter((itemTarea)=> itemTarea !== nombreTarea);
+    const borrarTarea = (nombreTarea) => {
+        let copiaTareas = tareas.filter((itemTarea) => itemTarea !== nombreTarea);
         setTareas(copiaTareas);
     }
 
     return (
         <>
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3 d-flex" controlId="tarea">
-                <Form.Control type="text" placeholder="Ingrese una tarea" onChange={(e)=> setTarea(e.target.value)} value={tarea}/>
-                <Button variant="primary" type="submit">Agregar</Button>
-            </Form.Group>
-        </Form>
-        <Lista tareas={tareas} borrarTarea={borrarTarea}></Lista>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3 d-flex" controlId="tarea">
+                    <Form.Control type="text" placeholder="Ingrese una tarea" onChange={(e) => setTarea(e.target.value)} value={tarea} />
+                    <Button variant="primary" type="submit">Agregar</Button>
+                </Form.Group>
+            </Form>
+            <Lista tareas={tareas} borrarTarea={borrarTarea}></Lista>
         </>
     );
 };
